b1e5b63ff31ffce7dad37027d9b730001576bb14
John Smith <unknown.prop@gmail.com>
2018-07-03 13:22:46 +0900

    just() 1. 인자로 넣은 데이터를 차례로 발행하기 위해 Observable 을 생성한다. 2. 실제 데이터 발행은 subscribe() 를 호출 후 시작한다. 3. 1 - 10 개의 값을 넣을 수 있다. 단 타입은 모두 동일해야 한다.

--- reactive-java/src/main/java/reactivejava/FirstExample.java
+++ reactive-java/src/main/java/reactivejava/FirstExample.java
@@ -4,7 +4,7 @@ import io.reactivex.Observable;
 
 public class FirstExample {
     public void emit() {
-        Observable.just("Hello", "RxJava 2!!")
+        Observable.just(1, 2, 3, 4, 5, 6)
                 .subscribe(data->System.out.println(data));
     }
 
