acbc56cbb27cb4dc4c7e0f50fe0720c6f78e08cc
John Smith <unknown.prop@gmail.com>
2018-07-03 13:50:07 +0900

    Observable create()
    just() 와는 달리 데이터를 발행하려면 onNext()를 직접 호출해야 하며 모든
    데이터를 발행 후 반드시 onComplete() 함수를 호출해야 한다.
    
    Observable<T> create(ObservableOnSubscribe<T> source>
    
    public interface ObservableOnSubscribe<T> {
      void subscribe(ObservableEmitter<T> e) throws Exception;
    }
    
    http://reactivex.io/documentation/operators/create.html

--- reactive-java/src/main/java/reactivejava/MainApplication.java
+++ reactive-java/src/main/java/reactivejava/MainApplication.java
@@ -1,13 +1,16 @@
 package reactivejava;
 
-import java.util.Observable;
-
 public class MainApplication {
     public static void main(String args[]) {
         FirstExample firstExample = new FirstExample();
         firstExample.emit();
 
+        // Observable subscribe example
         ObservableNotifications on = new ObservableNotifications();
         on.emit();
+
+        // Observable create example
+        ObservableCreateExample oc = new ObservableCreateExample();
+        oc.create();
     }
 }
---
+++ reactive-java/src/main/java/reactivejava/ObservableCreateExample.java
@@ -0,0 +1,18 @@
+package reactivejava;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+
+public class ObservableCreateExample {
+    public void create() {
+        Observable<Integer> source = Observable.create(
+                (ObservableEmitter<Integer> emitter)-> {
+                    emitter.onNext(100);
+                    emitter.onNext(200);
+                    emitter.onNext(300);
+                    emitter.onComplete();
+                });
+
+        source.subscribe(System.out::println);
+    }
+}
